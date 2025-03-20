6c5499b3de0de3791475ce2586e81f8637756616
HolyBarrel <magnus.allison@getmail.no>
2025-03-20 13:37:59 +0100

    Added loading of wq dataset

--- preprocessing/dataset_loader.py
+++ preprocessing/dataset_loader.py
@@ -2,10 +2,15 @@ import pandas as pd
 #import cupy as cp
 
 # Read the TSV file
-df = pd.read_csv('data/breast_cancer_wisconsin_original.tsv', sep='\t')
+bc_df = pd.read_csv('data/breast_cancer_wisconsin_original.tsv', sep='\t')
+
+wq_df = pd.read_csv('data\wine_quality_combined.tsv', sep='\t')
 
 # TODO: Add later when processing on GPU
 #data_cp = cp.array(df.values)
 
 # Display the first few rows to verify
-print(df.head())
+print(bc_df.head())
+
+print(wq_df.head())
+

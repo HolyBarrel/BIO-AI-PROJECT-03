cbb1742051f7a58466de46e720d3eee736832c99
flippi321 <90700334+flippi321@users.noreply.github.com>
2025-03-22 12:19:45 +0100

    Moved data output folder

--- XGB-Feature-Selection/generate_lookup.py
+++ XGB-Feature-Selection/generate_lookup.py
@@ -11,19 +11,19 @@ file = "banana"
 """
 
 if __name__ == "__main__":
-    file = "wine_quality_combined"
+    file = "1595_poker"
     
     # Create an instance of FeatureSelectionXGB
     selector = FeatureSelectionXGB(
         df_file_name=file, 
         target_column='target', 
         ignored_features=['wine_type'], 
-        print_per=100, 
+        print_per=10, 
         use_gpu=False, 
         show_lookup=True, 
         show_best=True,
         save_to_csv=True, 
-        output_location='preprocessing/output'
+        output_location='XGB-Feature-Selection/output'
     )
 
     # Run the feature selection experiment

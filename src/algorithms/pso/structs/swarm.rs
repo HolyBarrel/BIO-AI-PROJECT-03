f6d124a8f5565a1db474c95c33c8119c8913fc7a
flippi321 <90700334+flippi321@users.noreply.github.com>
2025-03-24 13:54:17 +0100

    Added an enum allowing us to switch the behaviour of the swarm

--- src/algorithms/pso/structs/swarm.rs
+++ src/algorithms/pso/structs/swarm.rs
@@ -1,18 +1,23 @@
 use crate::algorithms::pso::structs::particle::Particle; // Import particle struct
 pub use crate::structs::combination::Combination; // Import combination struct
 
+#[derive(Debug, Clone, Copy)]
+pub enum UpdateMode {
+    Global,
+    KNeighbor(usize),
+}
+
 #[derive(Debug, Clone)]
 pub struct Swarm {
     pub particles: Vec<Particle>, // Vector of particles in the swarm
     pub best_position: Vec<bool>, // Global best position found by the swarm
     pub best_loss: f64, // Global best loss found by the swarm
     pub size: usize, // Size of the swarm
+    pub mode: UpdateMode, // Update mode (global or k-neighbor)
 }
 
-// Functions
 impl Swarm {
-    // Create a new swarm with a given size and particle size
-    pub fn new(size: usize, particle_size: usize) -> Self {
+    pub fn new(size: usize, particle_size: usize, mode: UpdateMode) -> Self {
         let particles = (0..size).map(|_| Particle::new(particle_size)).collect::<Vec<Particle>>();
         let best_position = vec![false; particle_size]; // Initialize global best position
         let best_loss = f64::MAX; // Initialize global best loss to a large value
@@ -22,17 +27,16 @@ impl Swarm {
             best_position,
             best_loss,
             size,
+            mode,
         }
     }
 
-    // Print the swarm's information
     pub fn print(&self) {
         println!("Swarm Size: {:?}", self.size);
         println!("Global Best Position: {:?}", self.best_position);
         println!("Global Best Loss: {:?}", self.best_loss);
     }
 
-    // Update the global best position and loss based on the particles
     pub fn update_global_best(&mut self) {
         for particle in &self.particles {
             if particle.best_loss < self.best_loss {

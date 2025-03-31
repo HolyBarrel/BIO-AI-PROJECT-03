fde7f99461ce5310a736e437b86356611ee8d237
Pedro Cardona <pedropca@stud.ntnu.no>
2025-03-31 14:34:36 +0200

    Add output CSV files for multi-run validation and update data saving logic

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,6 @@ fn main() {
     //init();
     // Call the hello function from pso.rs
     //soo::single_output_optimization(["breast_cancer_wisconsin_original", "titanic", "wine_quality_combined"]);
-    soo::multi_run_validation("titanic");
+    soo::multi_run_validation("breast_cancer_wisconsin_original");
 
 }

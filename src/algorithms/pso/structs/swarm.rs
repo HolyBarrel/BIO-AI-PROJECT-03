2c74492f6706c5cdd0a95f798a6430f1de37ec07
flippi321 <90700334+flippi321@users.noreply.github.com>
2025-03-31 14:35:35 +0200

    Renamed loss to cost in particles, as this is more accurate

--- src/algorithms/pso/structs/swarm.rs
+++ src/algorithms/pso/structs/swarm.rs
@@ -7,7 +7,7 @@ use std::time::Instant;
 pub struct Swarm {
     pub particles: Vec<Particle>,   // Vector of particles in the swarm
     pub best_position: Vec<bool>,   // Global best position found by the swarm
-    pub best_loss: f64,             // Global best loss found by the swarm
+    pub best_cost: f64,             // Global best cost found by the swarm
     gen_to_best: usize,             // Number of generations until the best solution is found
     pub size: usize,                // Size of the swarm (number of particles)
     pub mode: UpdateMode,           // Update mode (global or k-neighbor)
@@ -18,7 +18,7 @@ impl Swarm {
         let mut swarm = Swarm {
             particles: Vec::new(),  // Empty at first
             best_position: vec![false; particle_size],
-            best_loss: f64::MAX, 
+            best_cost: f64::MAX, 
             size: size,
             gen_to_best: 0,
             mode,
@@ -32,14 +32,14 @@ impl Swarm {
     fn initialize_particles(&mut self, size: usize, particle_size: usize) {
         self.particles = (0..size).map(|_| Particle::new(particle_size)).collect::<Vec<Particle>>();
         self.best_position = vec![false; particle_size]; // Reset the best position
-        self.best_loss = f64::MAX;  // Reset the best loss
+        self.best_cost = f64::MAX;  // Reset the best cost
         self.gen_to_best = 0;       // Reset the generation count
     }
 
     pub fn update_global_best(&mut self, epoch: usize) {
         for particle in &self.particles {
-            if particle.best_loss < self.best_loss {
-                self.best_loss = particle.best_loss;
+            if particle.best_cost < self.best_cost {
+                self.best_cost = particle.best_cost;
                 self.best_position = particle.best_position.clone();
                 self.gen_to_best = epoch;
             }
@@ -50,7 +50,7 @@ impl Swarm {
     fn find_k_nearest_best(&self, particle_index: usize, k: usize) -> Vec<bool> {
         let mut neighbors = self.particles.iter().enumerate()
             .filter(|&(i, _)| i != particle_index) 
-            .map(|(i, p)| (i, p.best_loss, &p.best_position))
+            .map(|(i, p)| (i, p.best_cost, &p.best_position))
             .collect::<Vec<_>>();
         
         neighbors.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
@@ -77,7 +77,7 @@ impl Swarm {
             };
 
             particle.update_pos(best_position, w, c1, c2);
-            particle.update_loss(epsilon, combinations);
+            particle.update_cost(epsilon, combinations);
         }
     }
 
@@ -91,12 +91,12 @@ impl Swarm {
         for epoch in 0..epochs {
             self.step(epoch, w, c1, c2, epsilon, combinations);
             if epoch % 100 == 0 && print_performance {
-                println!("Epoch {}: Global best loss: {}", epoch, self.best_loss);
+                println!("Epoch {}: Global best cost: {}", epoch, self.best_cost);
                 println!("Epoch {}: Global best position: {:?}", epoch, self.best_position);
             }
         }
         if print_result {
-            println!("PSO algorithm completed. Final global best loss: {}", self.best_loss);
+            println!("PSO algorithm completed. Final global best cost: {}", self.best_cost);
             println!("Number of generations until best solution: {}", self.gen_to_best);
             println!("Final global best position: {:?}", self.best_position);
         }
@@ -105,22 +105,22 @@ impl Swarm {
 
     pub fn run_multiple(&mut self, w: f64, c1: f64, c2: f64, epsilon: f64, combinations: &Vec<Combination>, runs: usize, epoch_per_run: usize) {
         let mut model_solutions: Vec<Vec<bool>> = Vec::new();
-        let mut model_losses: Vec<f64> = Vec::new();
+        let mut model_costs: Vec<f64> = Vec::new();
         let mut gen_to_solutions: Vec<usize> = Vec::new();
         let mut runtimes: Vec<f64> = Vec::new();
     
         for _run in 0..runs {
             self.initialize_particles(self.particles.len(), self.best_position.len());
-
+    
             let start_time = Instant::now(); // Start timer
     
             self.perform_pso(w, c1, c2, epsilon, combinations, epoch_per_run, false, false);
-            
+    
             let elapsed_time = start_time.elapsed().as_secs_f64(); // Get elapsed time in seconds
             runtimes.push(elapsed_time); // Store runtime
     
             model_solutions.push(self.best_position.clone());
-            model_losses.push(self.best_loss);
+            model_costs.push(self.best_cost);
             gen_to_solutions.push(self.gen_to_best);
         }
     
@@ -131,17 +131,17 @@ impl Swarm {
             .sum::<f64>() / runs as f64;
         println!("AES: {:.6}", avg_gen_to_solution * self.size as f64);
     
-        // Calculate Average loss
-        let avg_loss: f64 = model_losses.iter().sum::<f64>() / runs as f64;
-        println!("MBF: {:.6}", 1.0 - avg_loss);
-
+        // Calculate Average Cost
+        let avg_cost: f64 = model_costs.iter().sum::<f64>() / runs as f64;
+        println!("MBF: {:.6}", 1.0 - avg_cost);
+    
         // Calculate and print average runtime
         let avg_runtime: f64 = runtimes.iter().sum::<f64>() / runs as f64;
         println!("Average Runtime: {:.6} seconds", avg_runtime);
-
-        // Give the best solution
+    
+        // Best solution
         println!("----------------------------------------");
-        let best_solution_index = model_losses
+        let best_solution_index = model_costs
             .iter()
             .enumerate()
             .min_by(|a, b| a.1.partial_cmp(b.1).unwrap())
@@ -157,5 +157,28 @@ impl Swarm {
             .map(|comb| comb.loss)
             .unwrap_or(f64::NAN)
         );
-    }
+        
+        let best_solution_loss = model_costs[best_solution_index];
+        println!("Best solution model loss: {}", best_solution_loss);
+    
+        // Mean solution with loss
+        println!("----------------------------------------");
+        let mean_solution_cost = model_costs.iter().copied().sum::<f64>() / model_costs.len() as f64;
+        let mean_solution_index = model_costs
+            .iter()
+            .enumerate()
+            .min_by(|a, b| a.1.partial_cmp(b.1).unwrap())
+            .map(|(i, _)| i)
+            .unwrap_or(0);
+        let mean_solution = model_solutions[mean_solution_index].clone();
+        println!(
+            "Mean solution: {:?}",
+            mean_solution.iter().map(|&b| b as u8).collect::<Vec<u8>>()
+        );
+        println!("Mean solution loss: {}", mean_solution_cost);
+    
+        // Find the corresponding model_costs for the best solution and mean solution
+        let mean_solution_cost_for_model = mean_solution_cost;
+        println!("Mean solution cost: {}", mean_solution_cost_for_model);
+    }    
 }
